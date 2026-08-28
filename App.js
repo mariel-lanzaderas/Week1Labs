1852fdcaf0fc63c027e57ea41e7911b7ca080609
Mariel P. Lanzaderas <mayellanzaderas@gmail.com>
2026-08-28 21:13:29 +0800

    Lab 2: Components and props with TaskCard

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import TaskCard from './components/TaskCard';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>My Tasks</Text>
+
+      <TaskCard title="Finish Lab 2" done={false} />
+      <TaskCard title="Read Chapter 3" done={true} />
+      <TaskCard title="Walk the dog" done={false} />
+      <TaskCard title="TEST 4TH TASK" done={true} />
     </View>
   );
 }
@@ -13,8 +17,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60,
+    paddingHorizontal: 16,
+    backgroundColor: '#FFFFFF',
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 12,
   },
 });
